64c4eaccac5b16b68b2539e252c40da112119e18
Michael Geng <fansgeng@gmail.com>
2026-04-03 17:59:19 +1100

    ch2 source github registry

--- ch2/tofu/live/sample-app/main.tf
+++ ch2/tofu/live/sample-app/main.tf
@@ -3,21 +3,24 @@ provider "aws" {
 }
 
 module "sample_app_1" {
-  source = "../../g
+  #source = "../../modules/ec2-instance"
+  source = "github.com/fansuogeng/devops-book//ch2/tofu/modules/ec2-instance?ref=1.0.0"
 
   name = "sample-app-tofu-1"
 
 }
 
 module "sample_app_2" {
-  source = "../../modules/ec2-instance"
+  #source = "../../modules/ec2-instance"
+  source = "github.com/fansuogeng/devops-book//ch2/tofu/modules/ec2-instance?ref=1.0.0"
 
   name = "sample-app-tofu-2"
 
 }
 
 module "sample_app_3" {
-  source = "../../modules/ec2-instance"
+  #source = "../../modules/ec2-instance"
+  source = "github.com/fansuogeng/devops-book//ch2/tofu/modules/ec2-instance?ref=1.0.0"
 
   name = "sample-app-tofu-3"
 
